c209d16dfd82e9cd371ed303245018ccd3156f43
debskiszymon <debski.szymon@gmail.com>
2020-09-26 12:57:39 +0200

    palindromy.py

--- palindromy.py
+++ palindromy.py
@@ -4,12 +4,11 @@ def palindromy(word):
     fuction will return True if a word is a palindrome
     else False
     '''
-    reverse = ""
-    for i in range(len(word) - 1, -1, -1):
-        reverse += word[i]
-    if word == reverse:
-        return True
-    else:
-        return False
+    ##reverse = ""
+    for i in range(len(word)):
+        if word[i] != word[len(word) - (i +1)]:
+            return False 
+    return True  
+
 print(palindromy("kajak"))
 print(palindromy("test"))
